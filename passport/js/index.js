ee2ebf0ea4cf530b9e68a65448de4a2ba0423092
604942578 <604942578@qq.com>
2016-07-08 08:10:08 +0800

    js

--- passport/js/index.js
+++ passport/js/index.js
@@ -16,6 +16,7 @@ var passport = {
 		},
 		
 		headRightMenu : function(){
+			
 			 $("#minbar").on("mouseover",".m_li",function(event){  // 右边黑蓝公用的效果
 			     $(this).addClass("m_lion");
 			     $(this).find(".r_av").show().stop().animate({opacity:1,right:"35px"},300);
@@ -24,14 +25,16 @@ var passport = {
 				 $(this).find(".r_av").stop().animate({opacity:0,right:"70px"},300,function(){$(this).hide()});
 			 });
 				 
-			 $('.m_tops').delegate('.top','click',function(){ //顶部 公用函数
+			 $('.m_tops').delegate('.top','click',function(e){ //顶部 公用函数
 				 $('html,body').stop().animate({scrollTop:'0px'},600);
+				 e.preventDefault();
 			 });
 			 
-			 $(".lha").delegate("h2","mouseenter",function(){
+			 $(".lha").delegate("h2","click",function(e){
 				 var i = $(this).index();
 				 $(this).addClass("on").siblings().removeClass("on");
 				 $("#lzone").children(".lfmo").eq(i).show().siblings().hide();
+				 e.preventDefault();
 			 });
 		},
 		captcha : function(){
