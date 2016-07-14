76dcb5899ab62e78cf758904ddb133bf5145eddb
wenjoying <wenjoying@126.com>
2016-07-15 00:38:15 +0800

    修改address.js 未完成

--- ucenter/js/address.js
+++ ucenter/js/address.js
@@ -8,60 +8,51 @@ function url() {
 var address = {// 首页js
 		'checkCon':function(){
 			if ($('form#con').size() > 0) {
-				$('form#con').submit(function(e) {
-					 e.preventDefault();
-		        }).validate({
-		        	rules:{
-		        		receiver_name: {
-		                    required: true,
-		                    minlength:2,
-		                    maxlength:20
-		                },
-		                detailed:{
-		                	required: true,
-		                	maxlength:50
-		                },
-		                tel:{
-		                	required: true,
-		                	mobile:true,
-		                },
-		                email: {
-		                	chkemail:true,
-		                },
-		                code:{
-		                	zipCode:true,
-		                },
-		        	},
-		        	messages:{
-		        		province_id:'',
-		        		city_id:'',
-		        	},
-		        	submitHandler: function(f) {
-		        		$.ajax({
-		                    type: 'post',
-		                    async: false,
-		                    dataType : 'json',
-		                    url: url()+'/Address/addPost',
-		                    data: $('#con').serialize(),
-		                    beforeSend: function() {
-		                        $('input[type=submit]').val('正在提交...').attr('disabled', true);
-		                    },
-		                    success: function(json) {
-		                        if (json.status) {
-		                        	alert('操作成功');
-		                            window.location.href = json.messages;
-		                        } else {
-		                            alert(json.messages);
-		                            $('input[type=submit]').val('确认提交').removeAttr('disabled');
-		                        }
-		                    }
-		                });
-		        		return false;
-		        	},
-		        	focusInvalid:true,
-		        	
-		        });
-				
+				$('form#con').submit(function(e) {//e.preventDefault();
+					if (!$(this).valid()) return false; 
+					var validForm = $(this).validate({
+			        	rules:{
+			        		receiver_name: {
+			                    required: true,
+			                    minlength:2,
+			                    maxlength:20
+			                },
+			                detailed:{
+			                	required: true,
+			                	maxlength:50
+			                },
+			                tel:{mobile:true,
+			                	required: true,
+			                	
+			                },
+			                code:{
+			                	zipCode:true,
+			                },
+			        	},
+					});//console.log(validForm.valid());
+					return false; 
+				    $('.error').html('');
+				    $(this).ajaxSubmit({
+				        type:"POST",
+				        beforeSubmit: function() {
+	                        $('input[type=submit]').val('正在提交...').attr('disabled', true);
+	                    },
+				        dataType:'json',
+				        async: false,
+	                    url: url()+'/Address/addPost',
+	                    data: $('#con').serialize(),
+				        success: function(json) {
+	                        if (json.status) {
+	                        	alert('操作成功');
+	                            window.location.href = json.messages;
+	                        } else {
+	                            alert(json.messages);
+	                            $('input[type=submit]').val('确认提交').removeAttr('disabled');
+	                        }
+	                    }
+				    });
+				    return false;
+		        })
 			}
 		},
 		initial:function(){
@@ -72,3 +63,5 @@ var address = {// 首页js
 jQuery(function(){
 	address.initial();
 })
+
+
