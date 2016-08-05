1eaebc6ed8e7abbefc7e0f3d4bdbf03fcd80db33
604942578 <604942578@qq.com>
2016-08-05 22:40:00 +0800

    个人中心的的整改

--- help/js/index.js
+++ help/js/index.js
@@ -7,7 +7,6 @@ var help = {
 		hostUrl:function(){
 			return location.protocol+'//'+location.host;
 		},
-		
 		cartLoad:function(){
 			 if ($('#tcar').size()>0) {
 				 var cart_flag = 0;  //购物车状态位 
@@ -32,46 +31,6 @@ var help = {
 						$(this).removeClass("hv");
 				});
 			 }
-			 
-			 if ($('#racar').size() > 0) {
-				 var flag = 0;
-				 $("#minarg").bind("mouseenter",function(){
-					 if (flag == 0) {
-						$.ajax({
-							url:help.url()+'/Home/getCart',
-							type: 'get',
-							jsonCallback: 'jsonCallback',
-							dataType:'jsonp',
-							beforeSend:function(){
-								$("#rxcar").html('<p class="alC c3 lh30">正在加载中...</p>');
-							},
-							success:function(data){
-								   $("#rxcar").html(data.html);
-								   flag = 1;
-							}
-						});
-					 }
-				 });
-			 }
-			 if ($("#rhist").size() > 0){
-				 var hflag = 0;
-				 $("#rhist").bind("mouseenter",function(){
-					   if (hflag==0) {
-					   	  $.ajax({
-							  url:help.url()+'/Home/getHistory',
-							  type:'post',
-							  dataType:'jsonp',
-							  beforeSend:function(){
-								 $("#hibx").html('<p class="alC">正在加载中...</p>');
-							  },
-							  success:function(data){
-				 				 $("#hibx").html(data.html);
-								 hflag = 1;
-							  }
-				 		  });
-					   }
-				 });
-			 }
 		 },
 		
 		headRightMenu : function(){
@@ -87,45 +46,13 @@ var help = {
 			 }).on("mouseout",'.nbt',function(){
 				$(this).removeClass("on");
 			 });
-			 
-			 $("#minbar").on("mouseover",".m_li",function(event){  // 右边黑蓝公用的效果
-			     $(this).addClass("m_lion");
-			     $(this).find(".r_av").show().stop().animate({opacity:1,right:"35px"},300);
-			 }).on("mouseout",'.m_li',function(){
-				 $(this).removeClass("m_lion");
-				 $(this).find(".r_av").stop().animate({opacity:0,right:"70px"},300,function(){$(this).hide()});
-			 });
-			 
+			 			 
 			 $("#bignav").hover(function(){ // 帮助中心菜单栏放进去的效果
 				 $("#lnav").show();
 		     },function() {
 		    	 $("#lnav").hide();
 		     });
-			 
-			 $('.m_tops').delegate('.top','click',function(event){ //顶部 公用函数
-				 $('html,body').stop().animate({scrollTop:'0px'},600);
-				 event.preventDefault();
-			 });
-			 
-			 if ($('.miao-header').size()>0) {
-				 var nav_top = 850;
-				 var has_nav = false;
-				 $(window).scroll(function(){
-			         var ns_top = $(window).scrollTop();
-					 if (ns_top>nav_top) {
-					     if (!has_nav) {
-					    	 $("#home_top").addClass("hs_fix");
-					         has_nav = true;
-					     }
-					 }else{
-					     if (has_nav) {
-					    	 $("#home_top").removeClass("hs_fix");
-				             has_nav = false;
-					     }
-					 }
-				 });
-			 }
-			 
+			
 		},
 		
 		captcha : function(){
