62e7900913c089e982256731d419ec4128fdacb8
Rabin <zhangkunch@qq.com>
2017-01-10 22:38:00 +0800

    修改登陆注册样式

--- passport/m/js/index.js
+++ passport/m/js/index.js
@@ -23,50 +23,35 @@
      return reg.test(tel);
  }
 
- var validEmail = function (email){
+ var validEmail = function (email) {
      var reg = /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/;
      return reg.test(email);
  }
 
- var alertMessage = function (msg) {
-     if ($('#timing-msg').size() == 0) {
-         var tag = '<div class="am-modal am-modal-no-btn pahod-alert am-modal-active" tabindex="-1" id="timing-msg">' + msg + '</div>';
-         $('body').append(tag);
-     }
-     setTimeout(function () {
-         $('#timing-msg').remove();
-     }, 2000);
- }
-
- var myalert = function (msg) {
-     $('#my-alert').remove();
-     var confirm = arguments[1] ? arguments[1] : '知道了';
-     var tag = '<div class="alert-explain am-modal" tabindex="-1" id="my-alert"><p>' + msg + '</p><div class="am-modal-footer"><span class="am-modal-btn">' + confirm + '</span></div></div>';
-     $('body').append(tag);
-     $('#my-alert').modal({'closeViaDimmer': false});
- }
-
  $(document).ready(function(){
      //登录提交页面
-     if ($('#login').size() > 0) {
-         $('#login').submit(function(e) {
-             var verify = $.trim($('input[name=verify]').val());
+     if ($('form.login').size() > 0) {
+         $('form.login').submit(function(event) {
+             //var verify = $.trim($('input[name=verify]').val());
              var username = $.trim($('input[name=username]').val());
              var password = $.trim($('input[name=password]').val());
-             if (!validMobile(username) || !validEmail(username)) {
-                 alertMessage('请输入手机号或邮箱');
-                 return;
+             if (!username) {
+                 layer.msg('请输入手机号或邮箱', {time: 1000});
+                 return false;
+             }
+             if (!validMobile(username) && !validEmail(password)) {
+                 layer.msg('手机号或邮箱格式不对', {time: 1000});
+                 return false;
              }
              if (!password) {
-                 alertMessage('请输入密码');
-                 return;
+                 layer.msg('请输入密码', {time: 1000});
+                 return false;
              }
              if (password.length < 6 || password.length > 18) {
-                 alertMessage('密码为6-18位数字和字母组合');
-                 return;
+                 layer.msg('密码为6-18位数字和字母组合', {time: 1000});
+                 return false;
              }
-
-             $.ajax({
+             $(this).ajaxSubmit({
                  type: 'post',
                  async: false,
                  dataType : 'json',
@@ -83,113 +68,50 @@
                      }
                  }
              });
-             e.preventDefault();
+             event.preventDefault();
          });
      }
 
      /*注册提交*/
-     if ($('.register-form-validate').size() > 0) {
-         $('.register-form-validate').submit(function(e) {
-             e.preventDefault();
-         }).validate({
-             rules: {
-                 phone: {
-                     required: true,
-                     mobile:true,
-                     remote: {
-                         url:hostUrl()+'/pc/register/validatePhone',
-                         type: 'post',
-                         dataType: 'json',
-                         data: {
-                             phone:function(json) {
-                                 return $('input[name=phone]').val();
-                             }
-                         }
-                     }
-                 },
-                 password: {
-                     required: true,
-                     rangelength:[6,20]
-                 },
-                 confirm_password: {
-                     required: true,
-                     equalTo: '#password'
-                 },
-                 verify: {
-                     required: true,
-                     remote: {
-                         url:hostUrl()+'/pc/register/validateVerify',
-                         type: 'post',
-                         dataType: 'json',
-                         data: {
-                             phone:function(json) {
-                                 return $('input[name=phone]').val();
-                             },
-                             verify:function(json) {
-                                 return $('input[name=verify]').val();
-                             }
-                         }
-                     }
-                 },
-                 parent_id: {
-                     remote: {
-                         url:hostUrl()+'/pc/register/validateParentId',
-                         type: 'post',
-                         dataType: 'json',
-                         data: {
-                             username:function(json) {
-                                 return $('input[name=parent_id]').val();
-                             }
-                         }
-                     }
-                 }
-             },
-             messages: {
-                 phone: {
-                     required: '请输入您的手机号码',
-                     mobile: '手机号码格式有误',
-                     remote: '手机号已注册'
-                 },
-                 password: {
-                     required: '请输入您的密码',
-                     rangelength: '密码长度只能在6-20位字符之间'
-                 },
-                 confirm_password: {
-                     required: '请再次输入密码',
-                     equalTo: '输入密码与原来不相同'
-                 },
-                 captcha: {
-                     required: '请输入验证码',
-                 },
-                 verify: {
-                     required: '请输入动态密码',
-                     remote: '动态密码无效'
-                 },
-                 parent_id: {
-                     remote: '当前推荐人无效'
-                 }
-             },
-             submitHandler: function(f) {
-                 $.ajax({
-                     type: 'post',
-                     async: false,
-                     dataType : 'json',
-                     url: hostUrl()+'/pc/register/doRegister',
-                     data: $('.register-form-validate').serialize(),
-                     beforeSend: function() {
-                         $('button[type=submit]').text('正在注册...').attr('disabled', true);
-                     },
-                     success: function(json) {
-                         if (json.status) {
-                             window.location.href = json.messages;
-                         } else {
-                             alert(json.messages);
-                             $('button[type=submit]').text('立即注册').removeAttr('disabled');
-                         }
-                     }
-                 });
+     if ($('form.login').size() > 0) {
+         $('form.register').submit(function(event) {
+             //var verify = $.trim($('input[name=verify]').val());
+             var username = $.trim($('input[name=username]').val());
+             var password = $.trim($('input[name=password]').val());
+             if (!username) {
+                 layer.msg('请输入手机号或邮箱', {time: 1000});
                  return false;
              }
+             if (!validMobile(username) && !validEmail(password)) {
+                 layer.msg('手机号或邮箱格式不对', {time: 1000});
+                 return false;
+             }
+             if (!password) {
+                 layer.msg('请输入密码', {time: 1000});
+                 return false;
+             }
+             if (password.length < 6 || password.length > 18) {
+                 layer.msg('密码为6-18位数字和字母组合', {time: 1000});
+                 return false;
+             }
+             $(this).ajaxSubmit({
+                 type: 'post',
+                 async: false,
+                 dataType : 'json',
+                 url: hostUrl()+'/m/register/doRegister',
+                 data: $('#login').serialize(),
+                 beforeSend: function() {
+                     $('[type=submit]').text('正在注册...').attr('disabled', true);
+                 },
+                 success: function(json) {
+                     if (json.status) {
+                         window.location.href = json.messages;
+                     } else {
+                         $('[type=submit]').text('注 册').removeAttr('disabled');
+                     }
+                 }
+             });
+             event.preventDefault();
          });
      }
 
